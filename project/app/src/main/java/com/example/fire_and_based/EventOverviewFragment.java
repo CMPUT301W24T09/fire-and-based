e69a9a08cec845327904d7f87b7f1d029570c08e
Tyler Beach <tabeach@ualberta.ca>
2024-03-06 12:29:51 -0700

    rename files

--- project/app/src/main/java/com/example/fire_and_based/EventOverviewFragment.java
+++ project/app/src/main/java/com/example/fire_and_based/EventOverviewFragment.java
@@ -9,6 +9,11 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class EventOverviewFragment extends Fragment {
 
