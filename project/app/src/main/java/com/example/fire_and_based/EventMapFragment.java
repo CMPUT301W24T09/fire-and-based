e4fc34f0a6a5b84b671a216e2a95af3ea720dd0e
sumayyat1 <tahersumayya@gmail.com>
2024-03-06 00:57:50 -0700

    Revert "Merge branch 'ui' of https://github.com/CMPUT301W24T09/fire-and-based into ui"
    This reverts commit 75c99662319cc97adfee6ea1f7ace27b5bc0fa76, reversing
    changes made to 28848c5b061e94ecc500db123a61ee8698ae1295.

--- project/app/src/main/java/com/example/fire_and_based/EventMapFragment.java
+++ project/app/src/main/java/com/example/fire_and_based/EventMapFragment.java
@@ -8,12 +8,17 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class EventMapFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.event_map_fragment, container, false);
+        return inflater.inflate(R.layout.event_map, container, false);
     }
 }
--- project/app/src/main/java/com/example/fire_and_based/MainActivity.java
+++ project/app/src/main/java/com/example/fire_and_based/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button button = findViewById(R.id.button2);
         button.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, AttendeeActivity.class);
+            Intent intent = new Intent(MainActivity.this, EventListActivity.class);
             startActivity(intent);
         });
 
