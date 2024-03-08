8e9655829525e76f3f040922c1fc5566eb140f35
TylerBeach <TylerAllenBeach@gmail.com>
2024-03-08 12:57:37 -0700

    add javadocs to Event/User

--- project/app/src/main/java/com/example/fire_and_based/User.java
+++ project/app/src/main/java/com/example/fire_and_based/User.java
@@ -2,11 +2,13 @@ package com.example.fire_and_based;
 
 import android.os.Parcel;
 import android.os.Parcelable;
-
 import androidx.annotation.NonNull;
-
 import java.util.ArrayList;
 
+/**
+ * Represents a user with personal information, device details, and events they are registered to.
+ * This class implements Parcelable to facilitate passing User objects between activities.
+ */
 public class User implements Parcelable {
     private String deviceID;
     private String userName;
@@ -17,26 +19,41 @@ public class User implements Parcelable {
     private String phoneNumber;
     private String email;
 
+    /**
+     * Constructs a new User with specified device ID, user name, profile picture, and a list of registered events.
+     *
+     * @param deviceID The unique identifier for the user's device.
+     * @param userName The user's chosen name.
+     * @param userRegisteredEvents A list of events the user has registered for.
+     * @param profilePicture The URL or resource identifier for the user's profile picture.
+     */
     public User(String deviceID, String userName, ArrayList<Event> userRegisteredEvents, String profilePicture){
         this.deviceID = deviceID;
-        this.profilePicture = profilePicture;
         this.userName = userName;
+        this.profilePicture = profilePicture;
         this.userRegisteredEvents = userRegisteredEvents;
-
     }
 
-
+    /**
+     * Internal constructor to create a User from a Parcel.
+     * This is used for parceling/unparceling.
+     *
+     * @param in The Parcel from which to read the user's data.
+     */
     protected User(Parcel in) {
         deviceID = in.readString();
         userName = in.readString();
         profilePicture = in.readString();
-        userRegisteredEvents = in.readArrayList(null);
+        userRegisteredEvents = in.readArrayList(Event.class.getClassLoader());
         firstName = in.readString();
         lastName = in.readString();
         phoneNumber = in.readString();
         email = in.readString();
     }
 
+    /**
+     * Creator required for classes that implement Parcelable.
+     */
     public static final Creator<User> CREATOR = new Creator<User>() {
         @Override
         public User createFromParcel(Parcel in) {
@@ -49,41 +66,43 @@ public class User implements Parcelable {
         }
     };
 
-    public String getDeviceID(){
+    // Accessor and mutator methods
+
+    public String getDeviceID() {
         return this.deviceID;
+    }
+
+    public void setDeviceID(String deviceID) {
+        this.deviceID = deviceID;
     }
 
     public String getUserName() {
         return this.userName;
     }
 
-    public ArrayList<Event> getUserEvents(){
-        return this.userRegisteredEvents;
-    }
-
-    public void setDeviceID(String deviceID){
-        this.deviceID = deviceID;
-    }
-
-    public void setUserName(String userName){
+    public void setUserName(String userName) {
         this.userName = userName;
     }
 
-    public void addEvent(Event event){
-        this.userRegisteredEvents.add(event);
-    }
-
-    public ArrayList<Event> getEvents(){
+    public ArrayList<Event> getUserEvents() {
         return this.userRegisteredEvents;
     }
 
-    public void setProfilePicture(String profilePicture){
-        this.profilePicture = profilePicture;
-    }
-    public void setUserRegisteredEvents(ArrayList<Event> eventList){
+    public void setUserRegisteredEvents(ArrayList<Event> eventList) {
         this.userRegisteredEvents = eventList;
     }
 
+    public void addEvent(Event event) {
+        this.userRegisteredEvents.add(event);
+    }
+
+    public String getProfilePicture() {
+        return this.profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
 
     public String getFirstName() {
         return firstName;
@@ -117,6 +136,8 @@ public class User implements Parcelable {
         this.email = email;
     }
 
+    // Parcelable implementation methods
+
     @Override
     public int describeContents() {
         return 0;
@@ -133,9 +154,4 @@ public class User implements Parcelable {
         dest.writeString(phoneNumber);
         dest.writeString(email);
     }
-
-    String getProfilePicture(){return this.profilePicture; }
-
-
-
 }
