3499dc22b0abd2d9ee72df7d63a6e0ed2fc57376
Tyler Beach <tabeach@ualberta.ca>
2024-03-31 02:51:23 -0600

    check in function working, sending check in location to database, reading from db and displaying on map

--- project/app/src/main/java/com/example/fire_and_based/FirebaseUtil.java
+++ project/app/src/main/java/com/example/fire_and_based/FirebaseUtil.java
@@ -834,11 +834,27 @@ public class FirebaseUtil {
 
 
 
-    // for adding a location that a user has checked in from
+    // for adding a location that a user has checked in from java docs are not real java docs cannot hurt me
     public static void sendCoordinatesToEvent(FirebaseFirestore db, Event event, GeoPoint geoPoint,OnSuccessListener<Void> successListener, OnFailureListener failureListener ){
         db.collection("events").document(event.getQRcode())
                 .update("checkInLocations", FieldValue.arrayUnion(geoPoint))
                 .addOnSuccessListener(successListener)
+                .addOnFailureListener(failureListener);
+    }
+
+
+    public static void getEventCheckInLocations(FirebaseFirestore db, Event event, OnSuccessListener<List<GeoPoint>> successListener, OnFailureListener failureListener) {
+        db.collection("events").document(event.getQRcode())
+                .get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    if (documentSnapshot.exists() && documentSnapshot.contains("checkInLocations")) {
+                        List<GeoPoint> checkInLocations = (List<GeoPoint>) documentSnapshot.get("checkInLocations");
+                        successListener.onSuccess(checkInLocations);
+                    } else {
+                        // Handle the case where the event doesn't have a 'checkInLocations' field or doesn't exist
+                        successListener.onSuccess(new ArrayList<>()); // Return an empty list as fallback
+                    }
+                })
                 .addOnFailureListener(failureListener);
     }
 
